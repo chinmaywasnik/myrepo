634685202e7a7ed8bf9cb9f03f92ea5d00cea88f
chinmaywasnik <114627993+chinmaywasnik@users.noreply.github.com>
2022-09-29 05:39:48 +0530

    Add files via upload

---
+++ webscript.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.write("Test app")
+
+
+#below line is working
+
+#st.markdown(f'<iframe src="https://spinzam.com/shot/embed/?idx=351885" width="500" height="500" frameborder="0" scrolling="no" style="max-width:100%; max-height:100vw;"></iframe>', unsafe_allow_html=True)
+
+st.markdown(f'<iframe src="https://spinzam.com/shot/embed/?idx=351919" width="500" height="500" frameborder="0" scrolling="no" style="max-width:100%; max-height:100vw;"></iframe>', unsafe_allow_html=True)
